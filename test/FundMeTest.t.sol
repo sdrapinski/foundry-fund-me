0fcf8ded5131e14d851975cdc112c891f0b9c768
sdrapinski <szymondrapinski@gmail.com>
2024-01-25 21:19:27 +0100

    tests

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "../lib/forge-std/src/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testDemo() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testPriceFeedConversionIsAccurate() public {}
+}
